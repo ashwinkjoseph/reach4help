12e4a28c7e610087de93a625fad147d55277d8a9
ashwinkjoseph <ashwinkjoseph@gmail.com>
2020-04-19 00:50:15 +0530

    fix: fetchig priviledged data into profile reducer
    adds the pirviledged information as well when fetching user data from firestore

--- web-client/src/modules/personalData/pages/ContentPage.tsx
+++ web-client/src/modules/personalData/pages/ContentPage.tsx
@@ -1,8 +1,7 @@
 import React, { ReactElement, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Redirect, Route, Switch, useLocation } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import { observeUserAction } from 'src/ducks/auth/actions';
-import { LoginLocation } from 'src/modules/login/pages/routes/LoginRoute/constants';
 import NotFoundRoute from 'src/pages/routes/NotFoundRoute';
 import { AppState } from 'src/store';
 
@@ -12,27 +11,15 @@ import { RoleInfoLocation } from './routes/RoleInfoRoute/constants';
 import RoleInfoRoute from './routes/RoleInfoRoute/RoleInfoRoute';
 
 const ContentPage = (): ReactElement => {
-  const user: firebase.User = useSelector((state: AppState) => state.auth.user);
   const loading = useSelector((state: AppState) => state.auth.loading);
   const dispatch = useDispatch();
-  const location = useLocation();
 
   useEffect((): any => observeUserAction(dispatch), [dispatch]);
 
-  if (loading && !user) {
+  if (loading) {
     return <>Loading</>;
   }
 
-  if (!user) {
-    return (
-      <Redirect
-        to={{
-          pathname: LoginLocation.path,
-          state: { redirectBack: location.pathname },
-        }}
-      />
-    );
-  }
   // FIXME This should check if the user has already filled the
   // Personal data from
   // if(auth.user.geolocation)
